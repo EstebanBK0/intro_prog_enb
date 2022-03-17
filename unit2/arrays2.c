2d61f8803cbcf9a77a3d38c5e979738f19a7afb0
EstebanBK0 <2109018@upy.edu.mx>
2022-03-17 08:41:21 -0600

    Class 17/03/2022

--- unit2/arrays2.c
+++ unit2/arrays2.c
@@ -6,4 +6,6 @@ int main (int argc, char** argv){
     float alturas[4] = {1.70, 1.80, 1.50, 1.68};
     //            index   0     1     2     3
     return 0;
+
+    printf("el valor de arr[2]", argv[2]);
 }
